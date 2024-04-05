8ef7ab8eb463ccc5ef877ad6893278a4d41c9c91
Atulbaral <atulbaral0110@gmail.com>
2024-04-05 17:38:53 +0545

    admin dashboard student and teacher list add

--- MIS_Classroom/Areas/Admin/Controllers/HomeController.cs
+++ MIS_Classroom/Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MIS_Classroom.Areas.Admin.Models;
+using MIS_Classroom.Models;
+using System.Linq;
 
 namespace MIS_Classroom.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly tattsContext _context;
+
+        public HomeController(tattsContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var teachers = _context.TechengineeMisTeachers.ToList();
+            var students = _context.TechengineeMisStudents.ToList();
+            var viewModel = new AdminDashboardViewModel
+            {
+                TechengineeMisTeachers = teachers,
+                TechengineeMisStudents = students
+            };
+            return View(viewModel);
         }
     }
 }
