347f805d31a8fb108a81b0ca0977cfaff770c860
Atulbaral <atulbaral0110@gmail.com>
2024-04-11 07:39:22 +0545

    teacher and student add admin with mojor changes in admin

--- MIS_Classroom/Areas/Admin/Controllers/HomeController.cs
+++ MIS_Classroom/Areas/Admin/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MIS_Classroom.Areas.Admin.Models;
-using MIS_Classroom.Models;
-using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using MIS_Classroom.Areas.Admin.Models; 
+using MIS_Classroom.Models; 
 
 namespace MIS_Classroom.Areas.Admin.Controllers
 {
@@ -12,21 +12,61 @@ namespace MIS_Classroom.Areas.Admin.Controllers
 
         public HomeController(tattsContext context)
         {
-            _context = context;                  
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            var teachers = _context.TechengineeMisTeachers.ToList();
+            var teachers = _context.TechengineeMisTeachers.Include(t => t.Subject).ToList();
             var students = _context.TechengineeMisStudents.ToList();
             var subjects = _context.TechengineeMisSubjects.ToList();
+            var questions = _context.TechengineeMisQuestions.Include(q => q.Subject).ToList();
+
+
             var viewModel = new AdminDashboardViewModel
             {
                 TechengineeMisTeachers = teachers,
                 TechengineeMisStudents = students,
-                TechengineeMisSubjects = subjects   
+                TechengineeMisSubjects = subjects,
+                TechengineeMisQuestions = questions
             };
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(string name, string email, int semester)
+        {
+          
+            var student = new TechengineeMisStudent
+            {
+                Name = name,
+                Email = email,
+                Semester = semester
+            };
+
+        
+            _context.TechengineeMisStudents.Add(student);
+            _context.SaveChanges();
+
+            return Ok(); 
+        }
+
+        [HttpPost]
+        public IActionResult AddTeacher(string name, string email, int subjectCode)
+        {
+           
+            var teacher = new TechengineeMisTeacher
+            {
+                Name = name,
+                Email = email,
+                SubjectCode = subjectCode
+            };
+
+          
+            _context.TechengineeMisTeachers.Add(teacher);
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
